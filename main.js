84613d323490cd964b57414bc37895c6f1e35249
abde21 <abdetam452@gmail.com>
2021-08-11 02:26:24 +0200

    start working Ex1

---
+++ main.js
@@ -0,0 +1,16 @@
+function getDetails(zName, zAge, zCountry) {
+    function namePattern(zName) {
+      
+    }
+    function ageWithMessage(zAge) {
+       
+    }
+    function countryTwoLetters(zCountry) {
+       
+    }
+    function fullDetails() {
+      
+    }
+    return fullDetails();  
+  }
+  
